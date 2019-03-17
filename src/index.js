7c53be215b13a6c63a48ffc3af76c45ab6364f45
Anish George <anishgeo@thoughtworks.com>
2019-03-17 15:31:03 +0530

    2.7 - Random working on web elements

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import { top, bottom } from "./footer";
 import makeButton from "./button";
 import { makeColorStyle } from "./button-styles";
 
+const button = makeButton("My first button!")
+
+document.body.appendChild(button)
+
 console.log(
   nav(),
   top,
   bottom,
-  makeButton("My first button!"),
   makeColorStyle("cyan")
 );
